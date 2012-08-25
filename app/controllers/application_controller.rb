03e2e94b0df33444f5e463ad5a18e0eafc78012a
Frankie Roberto <frankie@frankieroberto.com>
2012-08-26 00:19:00 +0100

    Cache for an hour except in development mode.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  before_filter :set_cache_control_header
+
+
+  private
+
+  def set_cache_control_header
+    response.headers['Cache-Control'] = 'public, max-age=3600' unless Rails.env == "development"
+  end
+
 end
