c4783f206ba66717d3dae96c53b73e35787129f1
Frankie Roberto <frankie@frankieroberto.com>
2012-08-28 18:03:38 +0100

    A few styling tweaks for the Little Printer

--- app/controllers/edition_controller.rb
+++ app/controllers/edition_controller.rb
@@ -3,6 +3,9 @@ class EditionController < ApplicationController
   def show
     @number = params[:delivery_count].to_i + 1
     @station = Station.find_by_number(@number)
+
+    @context = "little_printer"
+
     if @station
       render "stations/show"
     else
--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -2,6 +2,7 @@ class ExampleController < ApplicationController
 
   def show
     @station = Station.find("KBX")
+    @context = "little_printer"
     render "stations/show"
   end
 
