74c56fe65d4b7207e001a81d83b06241665c0494
Frankie Roberto <frankie@frankieroberto.com>
2012-09-26 19:11:41 +0100

    JSON update

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -6,9 +6,9 @@ class MetaController < ApplicationController
       "publication_api_version" => "1.0",
       "name" => "A handy guide to UK Rail Stations",
       "description" => "Useful and interesting information to help you survive long waits at the UK's busiest railway stations.",
-      "delivered_on" => "every week",
-      "send_timezone_info" => "false",
-      "send_delivery_count" => "true"
+      "delivered_on" => "every Monday",
+      "send_timezone_info" => true,
+      "send_delivery_count" => true
     }
 
     render :json => meta
