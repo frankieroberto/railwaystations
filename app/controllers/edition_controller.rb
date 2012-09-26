9a42d4a0fbf9cc4cf6210389d0b4245074728c3c
Frankie Roberto <frankie@frankieroberto.com>
2012-09-26 21:02:18 +0100

    bugfix

--- app/controllers/edition_controller.rb
+++ app/controllers/edition_controller.rb
@@ -3,10 +3,13 @@ class EditionController < ApplicationController
   def show
     @number = params[:delivery_count].to_i + 1
 
-    @local_delivery_time = DateTime.parse(params[:local_delivery_time])
+    if params[:local_delivery_time]
+      @local_delivery_time = DateTime.parse(params[:local_delivery_time])
+    end
+
     @station = Station.find_by_number(@number)
 
-    if @local_delivery_time.wday == 1 && @station
+    if params[:local_delivery_time] && @local_delivery_time.wday == 1 && @station
 
       @context = "little_printer"
 
